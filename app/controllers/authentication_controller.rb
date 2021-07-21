bec8d29c5f897c7adcc2ab1b55df1217a98cfb36
Gerald Bivens <geraldbivens@gmail.com>
2021-07-21 14:24:17 -0600

    Add logic to login action.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,4 +1,23 @@
 class AuthenticationController < ApplicationController
     def login
+        @user = User.find_by({ username: params[:username] })
+
+        if !@user
+            render json: { error: 'Invalid username' }, status: :unauthorized
+        else
+            if !@user.authenticate params[:password]
+                render json: { error: 'Invalid password' }, status: :unauthorized
+            else
+                payload = {
+                    user_id: @user.id
+                }
+
+                secret = 'topsecret'
+
+                token = JWT.encode payload, secret
+
+                render json: { token: token }, status: :created
+            end
+        end
     end
 end
